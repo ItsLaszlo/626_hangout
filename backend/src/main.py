ca67f507063e7ded3c4fbee552912b686b59b5e6
martham0 <martha060997@gmail.com>
2024-08-18 14:43:57 -0700

    update urls with current date range

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,10 +7,11 @@ import logging
 def main():
     """Main function to fetch, parse, and extract event data."""
     # Fetch the HTML content
-    san_gabriel_url = ['San Gabriel','https://www.sangabrielcity.com/calendar.aspx?Keywords=&startDate=07/01/2024&enddate=08/31/2024&CID=20&showPastEvents=true']
-    # san_gabriel_url = 'https://www.sangabrielcity.com/calendar.aspx?CID=20'
-    temple_url = ['Temple','https://www.ci.temple-city.ca.us/calendar.aspx?Keywords=&startDate=07/01/2024&enddate=08/31/2024&CID=23&showPastEvents=true']
-    # temple_url = 'https://www.ci.temple-city.ca.us/calendar.aspx?CID=23&Keywords=&startDate=&enddate=&'
+    san_gabriel_url = ['San Gabriel','https://www.sangabrielcity.com/calendar.aspx?CID=20']
+    temple_url = ['Temple','https://www.ci.temple-city.ca.us/calendar.aspx?CID=23&Keywords=&startDate=&enddate=&']
+# Dates range with events
+    # san_gabriel_url = ['San Gabriel','https://www.sangabrielcity.com/calendar.aspx?Keywords=&startDate=07/01/2024&enddate=08/31/2024&CID=20&showPastEvents=true']
+    # temple_url = ['Temple','https://www.ci.temple-city.ca.us/calendar.aspx?Keywords=&startDate=07/01/2024&enddate=08/31/2024&CID=23&showPastEvents=true']
     city_urls = [san_gabriel_url,temple_url]
     city_events = {}
     for city,url in city_urls:
@@ -18,9 +19,11 @@ def main():
         html_text = fetch_html(url)
         # Parse html
         html_soup = parse_html(html_text)
+        #extract event info from parsed html
         event_list = extract_events_sg(html_soup,city,url)
         city_events [city] = event_list
-    print(city_events.keys())
+    # print(city_events.keys())
+    # print(city_events)
 
 
 
