e27c5dc4f8e1d23dd020c0684963104b5cf642c1
martham0 <martha060997@gmail.com>
2024-08-29 13:40:13 -0700

    fetch data from API

--- frontend/626_hangout_fe/app/page.js
+++ frontend/626_hangout_fe/app/page.js
@@ -1,21 +1,48 @@
-// src/app/page.js
+'use client';
 
 import { Button, Typography, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { fetchData } from '../utils/api';
 
 export default function HomePage() {
-  return (
-   <Grid container direction="column" alignItems="center" justifyContent="center" style={{  paddingTop: '20px' }}>
-      <Grid item>
-      <Typography variant="h1" component="h1">
-        626 Hangout
-      </Typography>
-      </Grid>
-      <Grid item>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
-      </Grid>
-   </Grid>
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null); // Add error state
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const params = { 'city': 'pasadena' }; // Example query parameters
+        const result = await fetchData('626_hangout/events', params); // Use query parameters
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError(error.message); // Set error message in state
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <Grid container direction="column" alignItems="center" justifyContent="center" style={{ paddingTop: '20px' }}>
+      <Grid item>
+        <Typography variant="h1" component="h1">
+          626 Hangout
+        </Typography>
+      </Grid>
+      <Grid item>
+        {error ? (
+          <Typography variant="body1" color="error">Error: {error}</Typography>
+        ) : data ? (
+          <Typography variant="body1">Data: {JSON.stringify(data)}</Typography>
+        ) : (
+          <Typography variant="body1">Loading...</Typography>
+        )}
+      </Grid>
+      <Grid item>
+        <Button variant="contained" color="primary">
+          Click Me
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
---
+++ frontend/626_hangout_fe/utils/api.js
@@ -0,0 +1,18 @@
+const API_URL = 'http://localhost:5001'; // Flask API URL
+
+export const fetchData = async (endpoint, params = {}) => {
+   const queryString = new URLSearchParams(params).toString();
+   const api_url = `${API_URL}/${endpoint}?${queryString}`
+
+   try {
+    const response = await fetch(api_url);
+    if (!response.ok) {
+      const errorText = await response.text(); // Read error text from response
+      throw new Error(`Error ${response.status}: ${errorText}`); // Include status and error text
+    }
+    return await response.json();
+   } catch (error){
+   console.error('Error fetching data:', error);
+   throw error;
+   }
+ };
