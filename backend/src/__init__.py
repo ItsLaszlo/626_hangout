2abca9c24fd8b2f5de901b87208c2cf42faeacad
martham0 <martha060997@gmail.com>
2024-08-29 13:45:00 -0700

    setting up CORS

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -5,7 +5,8 @@ from .routes.events import sgv_event_api_bp
 def create_app():
     # Initialize Flask app
     app = Flask(__name__)
-
+    CORS(app)
+    # CORS(app, resources={r"/*": {"origins": "http://localhost:3000/"}})
     ## Enable CORS if frontend and backend are on different domains
     # CORS(app)
 
