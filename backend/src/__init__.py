2691279e2ddd1288ef55dcc474a463f15c849dea
martham0 <martha060997@gmail.com>
2024-08-29 13:45:20 -0700

    remove comments

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -6,9 +6,6 @@ def create_app():
     # Initialize Flask app
     app = Flask(__name__)
     CORS(app)
-    # CORS(app, resources={r"/*": {"origins": "http://localhost:3000/"}})
-    ## Enable CORS if frontend and backend are on different domains
-    # CORS(app)
 
     # Register routes
     app.register_blueprint(sgv_event_api_bp, url_prefix='/626_hangout')
