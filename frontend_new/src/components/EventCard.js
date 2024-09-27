36ae6d0acc08528ac4044c2a8b6a84761b71c8c1
martham0 <martha060997@gmail.com>
2024-09-27 14:32:56 -0700

    displaying event info

---
+++ frontend_new/src/components/EventCard.js
@@ -0,0 +1,44 @@
+import { Card, CardContent, Typography, CardMedia } from '@mui/material';
+import Link from 'next/link';
+
+export function EventCard({ event }) {
+  const eventImageUrl = event.image_url
+    ? event.image_url
+    : '/images/mm_logo.jpg'; // Use default image if event image is not available
+  return (
+    <Link href={event.url} passHref legacyBehavior>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: 'none' }}
+      >
+        <Card sx={{ marginBottom: 2 }}>
+          <CardContent>
+            <CardMedia
+              component="img"
+              image={eventImageUrl}
+              // image="/images/mm_logo.jpg"
+              alt="626 Hangout"
+              sx={{
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+            <Typography variant="h5" component="div">
+              {event.title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {event.date}
+            </Typography>
+            <Typography variant="body2">{event.description}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              Location: {event.city}
+            </Typography>
+          </CardContent>
+        </Card>
+      </a>
+    </Link>
+  );
+}
