43302a1b64bca2b43fef5573d3a8e8097390c24a
Martha <Yo@Jimmys-MacBook-Pro.local>
2024-10-31 15:39:07 -0700

    implement env file and remove urls

--- frontend_new/src/services/EventService.js
+++ frontend_new/src/services/EventService.js
@@ -1,16 +1,25 @@
 export async function fetchEvents() {
   try {
+    const baseUrl = process.env.NEXT_PUBLIC_READ_EVENT_API_URL;
+
+    if (!baseUrl) {
+      throw new Error("API URL is not defined in environment variables");
+    }
+
     const res = await fetch(
-      'http://127.0.0.1:5001/626_hangout/read_events?city=all', // ToDO: use ENV file
+      //make network request
+      `${baseUrl}read_events?city=all`,
       { next: { revalidate: 86400 } } // Revalidate once per day
     );
     if (!res.ok) {
+      // If request fails HTTP status codes 4xx and 5xx
       const errorText = await res.text();
-      throw new Error(`Error ${res.status}: ${errorText}`);
+      throw new Error(`Error ${res.status}: ${errorText}`); // Creates a error obj and stops execution and sends error to catch block
     }
     return res.json();
   } catch (error) {
-    console.error('Failed to fetch events:', error);
+    console.error("Failed to fetch events:", error);
     return [];
   }
 }
+// Purpose: server side function to fetch event data from API
